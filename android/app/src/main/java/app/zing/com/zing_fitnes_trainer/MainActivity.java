44d63aa1b47296b2ba7bea7b32631dc979a553e6
Ndjock Junior <ndjockjunior@gmail.com>
2019-10-06 18:02:05 +0100

    first commit

---
+++ android/app/src/main/java/app/zing/com/zing_fitnes_trainer/MainActivity.java
@@ -0,0 +1,13 @@
+package app.zing.com.zing_fitnes_trainer;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
