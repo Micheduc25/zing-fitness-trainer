25dc6f661f2627610a9c2216785055e721a57290
Achilis <tanguachilis@gmail.com>
2019-10-08 06:53:44 +0100

    modified textfield

--- lib/components/input.dart
+++ lib/components/input.dart
@@ -24,10 +24,15 @@ class Input_field extends StatelessWidget{
 
       child: TextField(
         decoration:InputDecoration(
+          alignLabelWithHint: true,
           icon: Icon(icon,size: MediaQuery.of(context).size.width/15, color: Color(0xff2f00ad),),
+
           labelText: hintText,
+
           labelStyle: TextStyle(color:Color(0xff2f00ad),),
+
           border:InputBorder.none,
+
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(20)),
             borderSide: BorderSide(color:Color(0xff6bd4ff)),
