a700630497d44ef4f5750c363bfe0312ca0fec3b
Ndjock Junior <ndjockjunior@gmail.com>
2019-10-08 21:36:31 +0100

    corrected some mistakes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zing_fitnes_trainer/components/InfoCard.dart';
 import 'package:zing_fitnes_trainer/components/InfoPages.dart';
 
 void main() => runApp(MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
       paramValue1: "05:00 PM",
       paramTitle2: "End time",
       paramValue2: "07:00 PM",
+      card: InfoCard(),
     );
   }
 }
