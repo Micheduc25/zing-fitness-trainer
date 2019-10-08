93269d092baa1f6c3ddb10ba369adfa331081369
Ndjock Junior <ndjockjunior@gmail.com>
2019-10-08 18:20:31 +0100

    added the bookings detail page

---
+++ lib/components/infoCard.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:zing_fitnes_trainer/utils/myColors.dart';
+import 'package:zing_fitnes_trainer/components/button.dart';
+
+class InfoCard extends StatelessWidget {
+  InfoCard({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    var colors = MyColors();
+    var size = MediaQuery.of(context).size;
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+              color: colors.gray,
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          padding: EdgeInsets.only(top: 10, bottom: 30, left: 10, right: 10),
+          width: size.width * 0.75,
+          child: Text("User info",
+              style: TextStyle(color: colors.deepBlue, fontSize: 13)),
+        ),
+        Transform.translate(
+          offset: Offset(0, 30),
+          child: Container(
+            decoration: BoxDecoration(
+                color: colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            width: size.width,
+            child: Column(
+              children: <Widget>[
+                _cardListItem(
+                    leading: Icon(
+                      Icons.location_on,
+                      color: colors.deepBlue,
+                      size: 29,
+                    ),
+                    title: "California",
+                    subtitle: '548 Market St, San Francisco, CA 94104'),
+                Divider(),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.calendar_today,
+                        color: colors.deepBlue,
+                        size: 25,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5),
+                        child: Text("24/08/2019"),
+                      )
+                    ],
+                  ),
+                ),
+                otherInfo("Session type", "Body Sculpting"),
+                otherInfo("Speciality", "Cardio"),
+                Container(
+                  decoration: BoxDecoration(
+                      color: colors.gray,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10))),
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 0),
+                  child: RichText(
+                    text: TextSpan(
+                        text: "Session Cost :",
+                        style: TextStyle(fontSize: 15, color: colors.textBlack),
+                        children: [
+                          TextSpan(
+                              text: "\$45",
+                              style: TextStyle(
+                                  color: colors.deepBlue,
+                                  fontWeight: FontWeight.w900))
+                        ]),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+Widget _cardListItem(
+    {Widget leading = null, String title = "", String subtitle = ""}) {
+  return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 3),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          leading,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 7),
+                child: Text(
+                  title,
+                  style: TextStyle(
+                      color: MyColors().textBlack,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w900),
+                ),
+              ),
+              Text(
+                subtitle,
+                maxLines: 3,
+                softWrap: true,
+                overflow: TextOverflow.fade,
+                style: TextStyle(color: MyColors().textBlack, fontSize: 13),
+              )
+            ],
+          ),
+        ],
+      ));
+}
+
+Widget otherInfo(title, content) {
+  return Container(
+    alignment: Alignment.centerLeft,
+    margin: EdgeInsets.only(left: 61),
+    padding: EdgeInsets.symmetric(vertical: 4),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(title,
+            style: TextStyle(fontSize: 13, color: MyColors().textBlack)),
+        Text(content,
+            style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w900,
+                color: MyColors().textBlack))
+      ],
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:zing_fitnes_trainer/screens/bookings_active/bookings_active.dart';
+import 'package:zing_fitnes_trainer/screens/bookingsDetail/bookingsDetail.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,6 +8,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BookingsPage();
+    return InfoPages(
+      title: "Bookings Details",
+      paramTitle1: "Start time",
+      paramValue1: "05:00 PM",
+      paramTitle2: "End time",
+      paramValue2: "07:00 PM",
+    );
   }
 }
