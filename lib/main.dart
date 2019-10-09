1611b6cc6d12c49ee336c6b72353653c35726713
Achilis <tanguachilis@gmail.com>
2019-10-09 10:23:48 +0100

    removed underscore from class names and modified login_signup page

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Login_SignUP();
+    return EmailVerification();
   }
 }
--- lib/screens/Login_SignUp/login_signUp.dart
+++ lib/screens/Login_SignUp/login_signUp.dart
@@ -26,57 +26,61 @@ class MyLoginSignUp extends StatelessWidget {
           child: ListView(
             children: <Widget>[
               Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
                 
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.fromLTRB(
-                      MediaQuery.of(context).size.width / 15,
-                      MediaQuery.of(context).size.height / 40,
-                      MediaQuery.of(context).size.width / 15,
-                      0),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Image.asset('./assets/images/logo.png',
-                                alignment: Alignment.centerLeft,
-                                height:
-                                    MediaQuery.of(context).size.height / 9.4,
-                                width: MediaQuery.of(context).size.width / 2.2),
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                    constraints: BoxConstraints(
+                      minHeight: MediaQuery.of(context).size.height - ( MediaQuery.of(context).size.height/5)
+                    ),
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.fromLTRB(
+                        MediaQuery.of(context).size.width / 15,
+                        MediaQuery.of(context).size.height / 40,
+                        MediaQuery.of(context).size.width / 15,
+                        0),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Image.asset('./assets/images/logo.png',
+                                  alignment: Alignment.centerLeft,
+                                  height:
+                                      MediaQuery.of(context).size.height / 9.4,
+                                  width: MediaQuery.of(context).size.width / 2.2),
 
-                            //
-                            //this is the area where we are going to do out conditional rendering
-                            //
-                            //The logic used here is that there is a provider class having a variable 
-                            //that controls which widget is to be displayed here
-                            //
-                            //initially the variable holds the Login() class and a Padding class to 
-                            // provide some space before the FootBg() element
-                            //
-                            //the Login() and SignUp() Widget(class) both Contains a flatButton with 
-                            //the Label Login, and Another  Flatbutton with the label SignUp
-                            //
-                            //if you click on the flatbutton with the label Login then the Login widget 
-                            //is what is displayed together with a Padding widget
-                            //
-                            //if you click on the flatbutton with the label SignUp, then then the SignUp 
-                            //widget is displayed together with a Padding widget
-                            //
-                            MultiProvider(
-                              providers: [
-                                ChangeNotifierProvider(
-                                  builder: (_) => Login_SignUp_Provider(),
-                                ),
-                              ],
-                              child: Consumer<Login_SignUp_Provider>(
-                                  builder: (context, data, child) =>
-                                      data.showCode),
-                            )
-                          ],
+                              //
+                              //this is the area where we are going to do out conditional rendering
+                              //
+                              //The logic used here is that there is a provider class having a variable 
+                              //that controls which widget is to be displayed here
+                              //
+                              //initially the variable holds the Login() class and a Padding class to 
+                              // provide some space before the FootBg() element
+                              //
+                              //the Login() and SignUp() Widget(class) both Contains a flatButton with 
+                              //the Label Login, and Another  Flatbutton with the label SignUp
+                              //
+                              //if you click on the flatbutton with the label Login then the Login widget 
+                              //is what is displayed together with a Padding widget
+                              //
+                              //if you click on the flatbutton with the label SignUp, then then the SignUp 
+                              //widget is displayed together with a Padding widget
+                              //
+                              MultiProvider(
+                                providers: [
+                                  ChangeNotifierProvider(
+                                    builder: (_) => LoginSignUpProvider(),
+                                  ),
+                                ],
+                                child: Consumer<LoginSignUpProvider>(
+                                    builder: (context, data, child) =>
+                                        data.showCode),
+                              )
+                            ],
                         ),
                       )
                     ],
