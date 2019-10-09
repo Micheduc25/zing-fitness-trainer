2127ad5b92fae963daddd145dbf1ffa1a265d715
Achilis <tanguachilis@gmail.com>
2019-10-09 21:25:11 +0100

    done with the front end of the trainer info page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:zing_fitnes_trainer/screens/Conversations/conversation.dart';
 import 'package:zing_fitnes_trainer/screens/Email_Verification/email_verification.dart';
+import 'package:zing_fitnes_trainer/screens/Trainer_Info_add/trainerInfoAdd.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,6 +9,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return EmailVerification();
+    return TrainerInfoAdd();
   }
 }
