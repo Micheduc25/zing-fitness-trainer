22d585e5aea60c7304bfde6a77ede219991e3e32
Ndjock Junior <ndjockjunior@gmail.com>
2019-10-09 07:51:57 +0100

    added th conversations page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:zing_fitnes_trainer/components/InfoCard.dart';
 import 'package:zing_fitnes_trainer/components/InfoPages.dart';
+import 'package:zing_fitnes_trainer/screens/Conversations/conversation.dart';
 import 'package:zing_fitnes_trainer/screens/Email_Verification/email_verification.dart';
 import 'package:zing_fitnes_trainer/screens/Login_SignUp/login_signUp.dart';
 import 'package:zing_fitnes_trainer/screens/bookings_active/bookings_active.dart';
@@ -12,6 +13,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return UserInfo();
+    return Conversations();
   }
 }
