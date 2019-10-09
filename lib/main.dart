8852728e7a0be0cb3586a0d932d249ddcdb01e79
Ndjock Junior <ndjockjunior@gmail.com>
2019-10-09 22:07:37 +0100

    added screenshots and deleted footerTestpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:zing_fitnes_trainer/screens/Conversations/conversation.dart';
 import 'package:zing_fitnes_trainer/screens/Email_Verification/email_verification.dart';
+import 'package:zing_fitnes_trainer/screens/Login_SignUp/login_signUp.dart';
 import 'package:zing_fitnes_trainer/screens/Trainer_Info_add/trainerInfoAdd.dart';
-=======
-import 'package:zing_fitnes_trainer/screens/Profile/profile.dart';
->>>>>>> c1423f8a379ce185a926316866fdd05b10c15202
+import 'package:zing_fitnes_trainer/screens/bookingsDetail/bookingsDetails.dart';
+import 'package:zing_fitnes_trainer/screens/bookings_active/bookings_active.dart';
+import 'package:zing_fitnes_trainer/screens/userInfo/userInfo.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,10 +12,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return TrainerInfoAdd();
-=======
-    return ProfilePage();
->>>>>>> c1423f8a379ce185a926316866fdd05b10c15202
+    return Login_SignUP();
   }
 }
