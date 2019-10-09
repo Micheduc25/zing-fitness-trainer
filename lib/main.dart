386ad0a975ca918cd6580c093960d7878b40a77a
Achilis <tanguachilis@gmail.com>
2019-10-09 22:25:09 +0100

    done pulling 22:23

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:zing_fitnes_trainer/screens/Conversations/conversation.dart';
 import 'package:zing_fitnes_trainer/screens/Email_Verification/email_verification.dart';
-import 'package:zing_fitnes_trainer/screens/Trainer_Info_add/trainerInfoAdd.dart';
-=======
 import 'package:zing_fitnes_trainer/screens/Profile/profile.dart';
->>>>>>> c1423f8a379ce185a926316866fdd05b10c15202
+import 'package:zing_fitnes_trainer/screens/Trainer_Info_add/trainerInfoAdd.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,10 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return TrainerInfoAdd();
-=======
     return ProfilePage();
->>>>>>> c1423f8a379ce185a926316866fdd05b10c15202
   }
 }
